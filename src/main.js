93bce4158896fd4d683acfbf3e897d4d755bed3e
Paris2020 <nelly.moseki@amazeelabs.com>
2023-12-12 17:57:03 +0200

    Feature - Update main.js and index.html file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueScrollTo from 'vue-scrollto'
 
-createApp(App).mount('#app')
+createApp(App).use(VueScrollTo, {
+    container: "body",
+    duration: 500,
+    easing: "ease",
+    offset: 0,
+    force: true,
+    cancelable: true,
+    onStart: false,
+    onDone: false,
+    onCancel: false,
+    x: false,
+    y: true
+}).mount('#app')
